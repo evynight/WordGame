01174232249047a87d2994eb4ba61a313f0b71a6
Evelynn <157990039+evynight@users.noreply.github.com>
2025-09-20 20:25:55 -0700

    Added Person.java

---
+++ Person.java
@@ -0,0 +1,25 @@
+public class Person
+{
+    public static void main(String[] args);
+    {
+        private String firstName;
+        private String lastName;
+
+        public void setFirstName(String f);
+        {
+            f = firstName;
+        }
+        public void setLastName(String l);
+        {
+            l = lastName;
+        }
+        public String getFirstName();
+        {
+            return firstName;
+        }
+        public String getLastName();
+        {
+            return lastName;
+        }
+    }
+}
