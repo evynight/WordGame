238302b08a73fbcea06416d1426a605150643157
Evelynn <lev21237959@maricopa.edu>
2025-10-05 02:27:14 -0700

    Added Money.java  class that implements the Award interface

---
+++ Money.java
@@ -0,0 +1,21 @@
+public class Money implements Award
+{
+    private final int MONEYWIN = 100;
+    private final int MONEYLOSE = 10;
+    
+    public int displayWinning(Players player, boolean guess)
+    {
+        if(guess == true)
+        {
+            System.out.println("Congratulations, " + player.getFirstName() +
+                            " " + player.getLastName() + "! You won!");
+            return MONEYWIN;
+        }
+        else
+        {
+            System.out.println("Oh, so sorry, " + player.getLastName() +
+                            " " + player.getLastName() + ". Seems you lost this time.");
+            return -MONEYLOSE;
+        }
+    }
+}
